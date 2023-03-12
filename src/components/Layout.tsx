c1d5118a7e5288f8a0cfde1f2732e6c50950fd53
whit33y <troppout@gmail.com>
2023-03-12 21:24:07 +0100

    Layout, header/footer added

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="text-3xl text-yellow-400">siema</div>
+      <div></div>
     </>
   );
 }
