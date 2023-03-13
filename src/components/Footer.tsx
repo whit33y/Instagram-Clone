73323c01743a66a5d9334c6d56060438ba1f3c3d
whit33y <troppout@gmail.com>
2023-03-13 15:00:46 +0100

    Footer with styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,33 @@
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="w-screen flex flex-col items-center text-gray-400 my-5">
+      <div>
+        <div className="flex mb-1">
+          <p className="footerLink">Meta</p>
+          <p className="footerLink">About</p>
+          <p className="footerLink">Blog</p>
+          <p className="footerLink">Jobs</p>
+          <p className="footerLink">Help</p>
+          <p className="footerLink">Api</p>
+          <p className="footerLink">Privacy</p>
+          <p className="footerLink">Terms</p>
+          <p className="footerLink">Top accounts</p>
+          <p className="footerLink">Locations</p>
+          <p className="footerLink">Instagram Lite</p>
+          <p className="footerLink">Contact uploading and non-users</p>
+        </div>
+        <div className="flex flex-row justify-center mb-3">
+          <p className="footerLink">Digital Colletcibles</p>
+          <p className="footerLink">Privacy Notice</p>
+          <p className="footerLink">Meta Verified</p>
+        </div>
+      </div>
+      <div className="flex">
+        <p className="text-xs px-1">English (UK)</p>
+        <p className="text-xs px-1">2023 Instagram from Meta</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
